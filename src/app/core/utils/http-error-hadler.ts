9968778f9fe07f9da89287564ed6cdc1a336081f
Sergey Malyuga <sergey@malyuga.ru>
2026-06-25 13:10:40 +0300

    feat(utils): добавить функцию для обработки http ошибок

---
+++ src/app/core/utils/http-error-hadler.ts
@@ -0,0 +1,7 @@
+import {HttpErrorResponse} from '@angular/common/http';
+import {throwError} from 'rxjs';
+
+export function httpErrorHandler(error: HttpErrorResponse) {
+  console.error(error);
+  return throwError(() => error.message);
+}
