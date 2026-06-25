3de62be2c7562e348b0a5d0cc5ed707751305b95
Sergey Malyuga <sergey@malyuga.ru>
2026-06-25 14:07:03 +0300

    feat(app): добавить селекторы
    - Возвращают офферы по городам
    - Возвращают текущий город

---
+++ src/app/store/app/selectors/app.selectors.ts
@@ -0,0 +1,21 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {OfferPreview} from '../../../core/models/offers';
+
+const selectOfferState = createFeatureSelector<AppState['offer']>('offer');
+const selectCityState = createFeatureSelector<AppState['currentCity']>('currentCity');
+
+export const selectOffersByCity = createSelector(
+  selectOfferState,
+  selectCityState,
+  (offerState, cityState) =>
+    Object.values(offerState.entities).filter((offer): offer is OfferPreview => {
+      if (offer === undefined) return false;
+      return offer.city.name === cityState.name;
+    })
+)
+
+export const selectCity = createSelector(
+  selectCityState,
+  state => state
+)
