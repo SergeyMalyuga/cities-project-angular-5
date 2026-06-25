cb5c54f22e519a18eddc8250fe3a54aba44cb29f
Sergey Malyuga <sergey@malyuga.ru>
2026-06-25 10:06:42 +0300

    feat(favorites): создать компонент страницы

---
+++ src/app/pages/favorites/favorites.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FavoritesComponent {
+
+}
