b24583b74f3b4bd1c705b1e0b49ad7b9fff9ff97
Sergey Malyuga <sergey@malyuga.ru>
2026-06-26 14:04:46 +0300

    feat(app): добавить проверку аутентификации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {RouterOutlet} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {AppState} from './core/models/app.state';
 import {loadOffers} from './store/offer/actions/offer.actions';
+import {checkAuth} from './store/user/actions/user.actions';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +16,6 @@ export class AppComponent implements OnInit {
 
   public ngOnInit(): void {
     this.store.dispatch(loadOffers());
+    this.store.dispatch(checkAuth());
   }
 }
