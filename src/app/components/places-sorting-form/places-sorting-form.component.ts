216081a0f1c5403a1ba0135ec45cdc7afc93b6f2
Sergey Malyuga <sergey@malyuga.ru>
2026-06-26 10:19:18 +0300

    feat(places-sorting-form): эмитить событие при выборе типа сортировки

--- src/app/components/places-sorting-form/places-sorting-form.component.ts
+++ src/app/components/places-sorting-form/places-sorting-form.component.ts
@@ -1,20 +1,30 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal} from '@angular/core';
 import {ToggleDirective} from '../../shared/directives/toggle.directive';
 import {NgClass} from '@angular/common';
 import {HoverTrackerDirective} from '../../shared/directives/hover-tracker.directive';
+import {SortType} from '../../core/constants/const';
+import {AccessibilityClickDirective} from '../../shared/directives/accessibility-click.directive';
 
 @Component({
   selector: 'app-places-sorting-form',
   imports: [
     ToggleDirective,
     NgClass,
-    HoverTrackerDirective
+    HoverTrackerDirective,
+    AccessibilityClickDirective
   ],
   templateUrl: './places-sorting-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlacesSortingFormComponent {
+  @Output() clicked = new EventEmitter<SortType>();
+  @Input({required: true}) currentSortType!: SortType;
+
+  protected readonly Object = Object;
+  protected readonly SortType = SortType;
+
   public isOpen = signal<boolean>(false);
+  public sortTypes = Object.values(SortType);
 
   public toggleMenu() {
     this.isOpen.set(!this.isOpen());
@@ -30,5 +40,10 @@ export class PlacesSortingFormComponent {
     if (!isHover) {
       this.closeMenu();
     }
+  }
+
+  public onClicked(sortType: SortType): void {
+    this.clicked.emit(sortType);
+    this.closeMenu();
   }
 }
