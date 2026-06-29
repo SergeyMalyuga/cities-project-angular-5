7122bd1af77980f5080e829eeca74bac2dc6bf24
Sergey Malyuga <sergey@malyuga.ru>
2026-06-29 14:38:49 +0300

    feat(favorite-offer): добавить обработку действий изменения статуса оффера

--- src/app/store/favorite-offer/favorite-offer.reducer.ts
+++ src/app/store/favorite-offer/favorite-offer.reducer.ts
@@ -1,7 +1,8 @@
 import {createEntityAdapter} from '@ngrx/entity';
 import {OfferPreview} from '../../core/models/offers';
 import {FavoriteOfferState} from '../../core/models/favorite-offer.state';
-import {createReducer} from '@ngrx/store';
+import {createReducer, on} from '@ngrx/store';
+import {toggleFavorite, toggleFavoriteFailure, toggleFavoriteSuccess} from './actions/favorite-offer.actions';
 
 export const favoriteOfferAdapter = createEntityAdapter<OfferPreview>();
 const initialStata: FavoriteOfferState = favoriteOfferAdapter.getInitialState({
@@ -10,4 +11,18 @@ const initialStata: FavoriteOfferState = favoriteOfferAdapter.getInitialState({
   success: null
 });
 
-export const favoriteOfferReducer = createReducer(initialStata);
+export const favoriteOfferReducer = createReducer(
+  initialStata,
+  on(toggleFavorite, state => ({
+    ...state, isLoading: true, success: null
+  })),
+  on(toggleFavoriteSuccess, (state, {offer}) => {
+    if (offer.isFavorite) {
+      return favoriteOfferAdapter.setOne(offer, {...state, isLoading: false, success: true, error: null})
+    }
+    return favoriteOfferAdapter.removeOne(offer.id, {...state, isLoading: false, error: null, success: true})
+  }),
+  on(toggleFavoriteFailure, (state, {error}) => ({
+    ...state, isLoading: false, error, success: false
+  }))
+);
