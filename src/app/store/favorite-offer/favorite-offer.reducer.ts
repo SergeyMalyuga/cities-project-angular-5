f79f73cbbc8e4d0f39b9f6af447711765dc5ca10
Sergey Malyuga <sergey@malyuga.ru>
2026-06-29 15:37:25 +0300

    feat(favorite-offer): добавить обработку действий загрузки офферов

--- src/app/store/favorite-offer/favorite-offer.reducer.ts
+++ src/app/store/favorite-offer/favorite-offer.reducer.ts
@@ -2,7 +2,12 @@ import {createEntityAdapter} from '@ngrx/entity';
 import {OfferPreview} from '../../core/models/offers';
 import {FavoriteOfferState} from '../../core/models/favorite-offer.state';
 import {createReducer, on} from '@ngrx/store';
-import {toggleFavorite, toggleFavoriteFailure, toggleFavoriteSuccess} from './actions/favorite-offer.actions';
+import {
+  loadFavoriteOffers, loadFavoriteOffersFailure, loadFavoriteOffersSuccess,
+  toggleFavorite,
+  toggleFavoriteFailure,
+  toggleFavoriteSuccess
+} from './actions/favorite-offer.actions';
 
 export const favoriteOfferAdapter = createEntityAdapter<OfferPreview>();
 const initialStata: FavoriteOfferState = favoriteOfferAdapter.getInitialState({
@@ -12,17 +17,27 @@ const initialStata: FavoriteOfferState = favoriteOfferAdapter.getInitialState({
 });
 
 export const favoriteOfferReducer = createReducer(
-  initialStata,
-  on(toggleFavorite, state => ({
-    ...state, isLoading: true, success: null
-  })),
-  on(toggleFavoriteSuccess, (state, {offer}) => {
-    if (offer.isFavorite) {
-      return favoriteOfferAdapter.setOne(offer, {...state, isLoading: false, success: true, error: null})
-    }
-    return favoriteOfferAdapter.removeOne(offer.id, {...state, isLoading: false, error: null, success: true})
-  }),
-  on(toggleFavoriteFailure, (state, {error}) => ({
-    ...state, isLoading: false, error, success: false
-  }))
-);
+    initialStata,
+    on(loadFavoriteOffers, state => ({
+      ...state, isLoading: true
+    })),
+    on(loadFavoriteOffersSuccess, (state, {offers}) =>
+      favoriteOfferAdapter.setAll(offers, {...state, isLoading: false, error: null})
+    ),
+    on(loadFavoriteOffersFailure, (state, {error}) => ({
+      ...state, isLoading: false, error
+    })),
+    on(toggleFavorite, state => ({
+      ...state, isLoading: true, success: null
+    })),
+    on(toggleFavoriteSuccess, (state, {offer}) => {
+      if (offer.isFavorite) {
+        return favoriteOfferAdapter.setOne(offer, {...state, isLoading: false, success: true, error: null})
+      }
+      return favoriteOfferAdapter.removeOne(offer.id, {...state, isLoading: false, error: null, success: true})
+    }),
+    on(toggleFavoriteFailure, (state, {error}) => ({
+      ...state, isLoading: false, error, success: false
+    }))
+  )
+;
