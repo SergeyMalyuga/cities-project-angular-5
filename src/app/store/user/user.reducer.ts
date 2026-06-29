b3190b1fbc823035f4f20c3b50df7c3afbbab9b0
Sergey Malyuga <sergey@malyuga.ru>
2026-06-29 10:25:19 +0300

    feat(user): добавить обработку действий
    - Аутентификация пользователя
    - Выход из учётной записи

--- src/app/store/user/user.reducer.ts
+++ src/app/store/user/user.reducer.ts
@@ -1,7 +1,15 @@
 import {UserState} from '../../core/models/user.state';
 import {AuthorizationStatus, DEFAULT_USER} from '../../core/constants/const';
 import {createReducer, on} from '@ngrx/store';
-import {checkAuth, checkAuthFailure, checkAuthSuccess} from './actions/user.actions';
+import {
+  checkAuth,
+  checkAuthFailure,
+  checkAuthSuccess,
+  login,
+  loginFailure,
+  loginSuccess,
+  logout, logoutFailure, logoutSuccess
+} from './actions/user.actions';
 
 const initialState: UserState = {
   user: DEFAULT_USER,
@@ -20,5 +28,23 @@ export const userReducer = createReducer(
   })),
   on(checkAuthFailure, (state, {error}) => ({
     ...state, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error
+  })),
+  on(login, state => ({
+    ...state, isLoading: true
+  })),
+  on(loginSuccess, (state, {user}) => ({
+    ...state, user, authorizationStatus: AuthorizationStatus.AUTH, isLoading: false, error: null
+  })),
+  on(loginFailure, (state, {error}) => ({
+    ...state, isLoading: false, authorizationStatus: AuthorizationStatus.UN_AUTH, error
+  })),
+  on(logout, state => ({
+    ...state, isLoading: true
+  })),
+  on(logoutSuccess, state => ({
+    ...state, user: DEFAULT_USER, isLoading: false, authorizationStatus: AuthorizationStatus.UN_AUTH, error: null
+  })),
+  on(logoutFailure, (state, {error}) => ({
+    ...state, isLoading: false, error
   }))
 )
