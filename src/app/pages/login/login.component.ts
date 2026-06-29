a8f696793a27fb25e1a6b4c5b17923ac7a854fe2
Sergey Malyuga <sergey@malyuga.ru>
2026-06-29 11:28:51 +0300

    feat(login): реализовать аутентификацию пользователя

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,62 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {AppRoute} from '../../core/constants/const';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {AppRoute, AuthorizationStatus} from '../../core/constants/const';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {Credentials} from '../../core/models/credentials';
+import {login} from '../../store/user/actions/user.actions';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {first} from 'rxjs';
+import {loadOffers} from '../../store/offer/actions/offer.actions';
+import {loadFavoriteOffers} from '../../store/favorite-offer/actions/favorite-offer.actions';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private store = inject(Store<AppState>);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
 
   protected readonly AppRoute = AppRoute;
+
+  public loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$')]],
+  });
+
+  public ngOnInit(): void {
+    this.store.select(selectAuthStatus).pipe(
+      first((authStatus) => authStatus === AuthorizationStatus.AUTH))
+      .subscribe(() => {
+        this.loginForm.reset();
+        this.store.dispatch(loadOffers());
+        this.store.dispatch(loadFavoriteOffers());
+        this.router.navigate([AppRoute.MAIN]);
+      });
+  }
+
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      const {email, password} = this.loginForm.value;
+      const credentials: Credentials = {email, password};
+      this.store.dispatch(login({credentials}));
+    }
+  }
+
+  public get passwordControl() {
+    return this.loginForm.get('password');
+  }
+
+  public get emailControl() {
+    return this.loginForm.get('email');
+  }
 }
