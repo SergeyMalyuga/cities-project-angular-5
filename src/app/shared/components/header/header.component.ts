bffbe1629e4370ce7369b28617b9f9ec59b03865
Sergey Malyuga <sergey@malyuga.ru>
2026-06-29 10:28:35 +0300

    feat(header): добавить отображение количества избранных офферов и email пользователя
    - Информация отображается для аутентифицированных пользователей

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {AppRoute} from '../../../core/constants/const';
+import {AppRoute, AuthorizationStatus} from '../../../core/constants/const';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {selectAuthStatus, selectUserEmail} from '../../../store/user/selectors/user.selectors';
+import {selectFavoriteOffersTotal} from '../../../store/favorite-offer/selectors/favorite-offer.selectors';
+import {logout} from '../../../store/user/actions/user.actions';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +16,19 @@ import {AppRoute} from '../../../core/constants/const';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  private store = inject(Store<AppState>);
 
   protected readonly AppRoute = AppRoute;
+
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  public userEmail = this.store.selectSignal(selectUserEmail);
+  public favoriteOffersTotal = this.store.selectSignal(selectFavoriteOffersTotal);
+
+  public isAuthorized = computed(() => this.authStatus() === AuthorizationStatus.AUTH);
+
+  public logout() {
+    if (this.isAuthorized()) {
+      this.store.dispatch(logout());
+    }
+  }
 }
