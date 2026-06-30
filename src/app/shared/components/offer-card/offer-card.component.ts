672616e9ad227522d1d68ea2d5ac99385583b0fb
Sergey Malyuga <sergey@malyuga.ru>
2026-06-30 12:38:28 +0300

    feat(favorites): добавить загрузку оффера, комментариев и предложений рядом

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,4 +1,13 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output, signal} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  signal
+} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
 import {NgClass, TitleCasePipe} from '@angular/common';
 import {HoverTrackerDirective} from '../../directives/hover-tracker.directive';
@@ -6,13 +15,18 @@ import {OfferService} from '../../../core/services/offer.service';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../../core/models/app.state';
 import {selectIsFavoriteOffersLoading} from '../../../store/favorite-offer/selectors/favorite-offer.selectors';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
+import {first, tap} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
     TitleCasePipe,
     HoverTrackerDirective,
-    NgClass
+    NgClass,
+    RouterLink
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -21,9 +35,12 @@ export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
   @Input() isFavoritesPage = false;
   @Output() hovered = new EventEmitter<OfferPreview | null>();
+  @Output() toggled = new EventEmitter<void>();
 
   private offerService = inject(OfferService);
   private store = inject(Store<AppState>);
+  private destroyRef = inject(DestroyRef);
+
   public isLoading = this.store.selectSignal(selectIsFavoriteOffersLoading);
 
   protected readonly Math = Math;
@@ -37,6 +54,13 @@ export class OfferCardComponent {
   }
 
   public toggleFavorite() {
-    this.offerService.toggleFavorite(this.offer.id, this.offer.isFavorite);
+    this.offerService.toggleFavorite(this.offer.id, this.offer.isFavorite)
+      .pipe(first(success => success !== null), tap(success => {
+        if (success) {
+          this.toggled.emit();
+        }
+      }), takeUntilDestroyed(this.destroyRef)).subscribe();
   }
+
+  protected readonly AppRoute = AppRoute;
 }
