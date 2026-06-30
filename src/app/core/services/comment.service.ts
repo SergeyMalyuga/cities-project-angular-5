9e59efe4f27a4e5d731dbd9bce2fa0b8a8864f1e
Sergey Malyuga <sergey@malyuga.ru>
2026-06-30 12:36:27 +0300

    feat(comment): добавить сервис комментариев

---
+++ src/app/core/services/comment.service.ts
@@ -0,0 +1,24 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Comment} from '../models/comments';
+import {Observable} from 'rxjs';
+import {APIRoute, BASE_URL} from '../constants/const';
+import {defaultHttpOperators} from '../utils/rjxs-operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  private http = inject(HttpClient);
+
+  public getComments(offerId: string): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${BASE_URL}/${APIRoute.COMMENTS}/${offerId}`).pipe(...defaultHttpOperators<Comment[]>());
+  }
+
+  public postComment(offerId: string, comment: string, rating: number): Observable<Comment> {
+    return this.http.post<Comment>(`${BASE_URL}, ${APIRoute.COMMENTS}/${offerId}`, {
+      comment,
+      rating
+    }).pipe(...defaultHttpOperators<Comment>());
+  }
+}
