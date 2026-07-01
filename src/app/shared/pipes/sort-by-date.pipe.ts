451dec4d420f4856c3c61a1ff43ea4eb42ee60bd
Sergey Malyuga <sergey@malyuga.ru>
2026-07-01 10:27:36 +0300

    feat(shared): добавить pipe сортировки по дате

---
+++ src/app/shared/pipes/sort-by-date.pipe.ts
@@ -0,0 +1,22 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {HasDate} from '../../core/models/has-date.model';
+
+@Pipe({
+  name: 'sortByDate'
+})
+export class SortByDatePipe implements PipeTransform {
+  transform<T extends HasDate>(values: T[]): T[] {
+    if (!values || values.length === 0) {
+      return [];
+    }
+    return [...values].sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+
+      const validDateA = Number.isNaN(dateA) ? 0 : dateA;
+      const validDateB = Number.isNaN(dateB) ? 0 : dateB;
+
+      return validDateB - validDateA;
+    });
+  }
+}
